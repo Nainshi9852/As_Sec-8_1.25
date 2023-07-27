5741da3bd1dab06036ce0f4a6af6fef230ac50ec
nainshisingh985@gmail.com <nainshisingh985@gmail.com>
2023-07-27 21:13:54 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+            Console.ReadKey();
+        }
+        public static void runApp()
+        {
+            string[] students = new string[5];
+            students[0] = "Rahul";
+            students[1] = "Sam";
+            students[2] = "Aman";
+            students[3] = "Sahil";
+            students[4] = "Sunny";
+        
+            int currElem = 0;
+            printViaRecursion(students, currElem);
+        }
+
+        public static void printViaRecursion(string[] arr, int currElem)
+        {
+            Console.WriteLine(arr[currElem]);
+            if (currElem < arr.Length - 1)
+                printViaRecursion(arr, currElem + 1);
+        }
+
+    }
+}
